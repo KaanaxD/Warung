04bc85ce9d4a38103b541c11149ed8b04357ab4a
LemenAir <lmarkabmaulanam348@gmail.com>
2026-06-11 12:37:54 +0800

    Rate limiter

---
+++ src/config/rateLimiter.ts
@@ -0,0 +1,26 @@
+import rateLimit from "express-rate-limit";
+
+export default function rateLimiter(){
+    return{
+        generalRateLimiter: rateLimit({
+            windowMs:60000 * 15,
+            limit: 50,
+            legacyHeaders: false,
+            standardHeaders: true,
+            message:{
+                status: 429,
+                message: "to many request, try again later"
+            }
+        }),
+        auhtRateLimiter: rateLimit({
+            windowMs:60000 * 15,
+            limit: 10,
+            legacyHeaders: false,
+            standardHeaders: true,
+            message:{
+                status: 429,
+                message: "apasih obok obok login panel, coba lagi nanti ye"
+            }
+        })
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,17 @@ import { itemRouter } from "./routers/itemRouter"
 import { auth } from "./middlewares/auth"
 import path from "node:path"
 import { veiwRouter } from "./routers/viewRouter"
+import rateLimiter from "./config/rateLimiter"
+const {generalRateLimiter,auhtRateLimiter} = rateLimiter()
 
 const app = express()
 const port = process.env.PORT
 
 app.use(express.urlencoded())
 app.use(express.json())
+
+app.use('/api',generalRateLimiter)
+app.use('/api/auth',auhtRateLimiter)
 
 app.use('/api/img',express.static(path.join(__dirname,"..","uploads")))
 app.use('/api/auth',authRouter)
